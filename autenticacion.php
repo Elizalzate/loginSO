5265ac8ef1707150b847647b3d59e63d10d46fe0
Administrador@DOMINIO.SO <Administrador@DOMINIO.SO>
2021-09-04 03:22:50 +0200

    ·nitido·

--- autenticacion.php
+++ autenticacion.php
@@ -1,21 +1,23 @@
 <?php
     $ldapuser  = $_POST["usuario"];
     $ldappass = $_POST["password"];
-    $ldapconn = ldap_connect("dominio.so") or die("No fue posible ingresar.");
+    $ldapconn = ldap_connect("ldap://dominio.so") or die("No fue posible ingresar.");
 
     if ($ldapconn) 
     {
         $ldapbind = ldap_bind($ldapconn, $ldapuser, $ldappass);
         if ($ldapbind) 
         {
+            echo "nitido";
             header('success.html');
         }
         else 
         {
+            echo $ldapbind.$ldapuser.$ldappass;
             echo "No se pudo.";
         }
     }
-    $Result = ldap_search($ldapconn, "OU=IT,DC="Domain",DC=corp", "(samaccountname=$ldaprdn)", array("dn"));
-    $data = ldap_get_entries($ldapconn, $Result);
-    print_r($data);
+    // $Result = ldap_search($ldapconn, "OU=IT,DC="Domain",DC=corp", "(samaccountname=$ldaprdn)", array("dn"));
+    // $data = ldap_get_entries($ldapconn, $Result);
+    // print_r($data);
 ?>
