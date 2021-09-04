fbb1c0a7d43abe60ed7acc3977e98670f9497a2c
Elizabeth Alzate <67381475+Elizalzate@users.noreply.github.com>
2021-09-04 17:53:37 -0500

    se agrega sweetalert

--- autenticacion.php
+++ autenticacion.php
@@ -8,19 +8,18 @@
         $ldapbind = ldap_bind($ldapconn, $ldapuser, $ldappass);
         if ($ldapbind) 
         {
-            echo "nitido";
             header('Location: success.html');
         }
         else 
         {
             header('Location: index.html');
             echo'<script type="text/javascript">
-            alert("Credenciales incorrectas");
-            window.location.href="index.html";
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "¡Credenciales incorrectas!",
+              })
             </script>';
         }
     }
-    // $Result = ldap_search($ldapconn, "OU=IT,DC="Domain",DC=corp", "(samaccountname=$ldaprdn)", array("dn"));
-    // $data = ldap_get_entries($ldapconn, $Result);
-    // print_r($data);
 ?>
