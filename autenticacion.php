c648a7b8f060a4726e7d0c4bd6a4717277f91eef
Elizabeth Alzate <67381475+Elizalzate@users.noreply.github.com>
2021-09-04 02:50:34 -0500

    Prueba autenticación

---
+++ autenticacion.php
@@ -0,0 +1,21 @@
+<?php
+    $ldapuser  = $_POST["usuario"];
+    $ldappass = $_POST["password"];
+    $ldapconn = ldap_connect("dominio.so") or die("No fue posible ingresar.");
+
+    if ($ldapconn) 
+    {
+        $ldapbind = ldap_bind($ldapconn, $ldapuser, $ldappass);
+        if ($ldapbind) 
+        {
+            header('success.html');
+        }
+        else 
+        {
+            echo "No se pudo.;
+        }
+    }
+    $Result = ldap_search($ldapconn, "OU=IT,DC="Domain",DC=corp", "(samaccountname=$ldaprdn)", array("dn"));
+    $data = ldap_get_entries($ldapconn, $Result);
+    print_r($data);
+?>
