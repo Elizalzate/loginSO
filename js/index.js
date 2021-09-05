9ed4c478a72c2d0b920b0209d8ea8c42965634d7
Elizabeth Alzate <67381475+Elizalzate@users.noreply.github.com>
2021-09-04 19:06:16 -0500

    validaciones js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+let ingresar = document.getElementById('btnIngresar');
+let usuario = document.getElementById('usuario').value;
+ingresar.addEventListener("click",
+(e)=>{
+    if(document.getElementById('usuario').value==='' || document.getElementById('password').value===''){
+        e.preventDefault();
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "No pueden haber campos vacíos",
+        });  
+    }
+    if( document.getElementById('password').value.length < 8){
+        e.preventDefault();
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "La contraseña debe tener mínimo 8 carácteres.",
+        });
+    }
+
+});
 const credIncorrectas = () =>{
     Swal.fire({
         icon: "error",
