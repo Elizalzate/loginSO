87f47fba8f0d4d959ccbe429d32d98aeeabdfaf5
Elizabeth Alzate <67381475+Elizalzate@users.noreply.github.com>
2021-09-04 19:43:00 -0500

    exception bind

--- autenticacion.php
+++ autenticacion.php
@@ -1,11 +1,11 @@
 <?php
     $ldapuser  = $_POST["usuario"];
     $ldappass = $_POST["password"];
-    $ldapconn = @ldap_connect("ldap://dominio.so") or die("No fue posible conectarse al dominio.");
+    $ldapconn = ldap_connect("ldap://dominio.so") or die("No fue posible conectarse al dominio.");
 
     if ($ldapconn) 
     {
-        $ldapbind = ldap_bind($ldapconn, $ldapuser, $ldappass);
+        $ldapbind = @ldap_bind($ldapconn, $ldapuser, $ldappass);
         if ($ldapbind) 
         {
             header('Location: success.html');
